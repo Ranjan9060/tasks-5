6ed1c4d0755372476c5b08bd20df7c1b1edaccd1
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-09 18:50:08 -0400

    Add files via upload

---
+++ 1. Write a program to print MySirG N times on the screen.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+
+    printf("Enter the Number :");
+
+    scanf("%d",&num);
+
+    for(i=1;i<=num;i++)
+    {
+        printf(" MySirG\n");
+    }
+
+    return 0;
+
+}
