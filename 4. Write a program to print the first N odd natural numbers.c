891215cd280bf75bd861501dbf21c53a69355957
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-09 19:08:00 -0400

    Add files via upload

---
+++ 4. Write a program to print the first N odd natural numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+
+    printf("Enter a Number :");
+
+    scanf("%d",&num);
+
+    for(i=1;i<=num;i++)
+    {
+
+        printf("%d ",i*2-1);
+
+    }
+
+    return 0;
+}
