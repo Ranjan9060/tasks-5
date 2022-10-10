6fff1c96dd352d61e33c1669c78b7fdcf607840b
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-10 02:22:01 -0400

    Add files via upload

---
+++ 8. Write a program to print squares of the first N natural numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+
+    printf("Enter a Number :");
+
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+        printf("%d ",i*i);
+    }
+
+    return 0;
+
+}
